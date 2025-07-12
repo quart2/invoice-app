dc6dbe9317e077c03f9b1769ee3c6b8e196e8dfb
quratulain <abc@gmail.com>
2025-07-12 18:44:10 +0500

    Add root route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Invoice API!");
+});
+
 app.use("/api/invoices", invoiceRoutes);
 
 connectDB().then(() => {
